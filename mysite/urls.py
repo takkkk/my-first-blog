407ecee9cb477807efb2b4a928c9f725225e62a6
taka2 <taka314@live.com>
2015-12-15 16:29:26 +0800

    Some error fix.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
